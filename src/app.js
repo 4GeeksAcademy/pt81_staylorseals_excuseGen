6d40ad35a7ac0701097ce9302417ea2312bd903d
Sierrena Taylor-Seals <sierrena.taylor@outlook.com>
2025-05-23 23:21:59 +0000

    excuse generator finished

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'hugged', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+let why = ['because the sun was shining', 'because the Hungry Caterpillar ate eveything', 'because I said "Hi"', 'because everyone won a free pizza'];
+
+function getRandomItem(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+function generateExcuse() {
+  let excuse = `${getRandomItem(who)} ${getRandomItem(action)} ${getRandomItem(what)} ${getRandomItem(when)} ${getRandomItem(why)}.`;
+  document.getElementById("excuse").innerText = excuse;
+  console.log(excuse); 
+}
+
+generateExcuse();
+
+document.getElementById("excuse").onclick = generateExcuse
+}
